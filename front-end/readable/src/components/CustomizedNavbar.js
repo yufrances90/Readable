eb99a815da2bfe8b383cae8b7f486590f892a77c
yufrances90 <yufrances90@gmail.com>
2018-10-18 14:41:26 -0700

    Made several changes
    - chore: Installed Material UI Icons
    - feat: Created customized navbar

--- front-end/readable/src/components/CustomizedNavbar.js
+++ front-end/readable/src/components/CustomizedNavbar.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { LibraryBooks } from '@material-ui/icons';
+
 class CustomizedNavbar extends Component {
     render() {
         return (
             <div>
-                Hello from CustomizedNavbar
+                <AppBar position="static" className="app-navbar">
+                    <Toolbar>
+                        <LibraryBooks style={{marginRight: '0.2em'}} />
+                        <Typography variant="title" color="inherit">
+                            Readable
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
             </div>
         );
     }

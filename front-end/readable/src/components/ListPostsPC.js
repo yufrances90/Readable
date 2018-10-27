b0ce65ccc8a37e37f30f3a5bd3c6d5bb44df89dc
yufrances90 <yufrances90@gmail.com>
2018-10-26 17:17:07 -0700

    refactor: Refactored the header component on the PCategory page into new component

--- front-end/readable/src/components/ListPostsPC.js
+++ front-end/readable/src/components/ListPostsPC.js
@@ -1,74 +1,24 @@
 import React, { Component } from 'react';
 
-import {
-    Typography,
-    Divider,
-    IconButton,
-    Tooltip,
-    FormControl,
-    InputLabel,
-    Select,
-    MenuItem
-} from '@material-ui/core';
-import { Sort } from '@material-ui/icons';
+import { Divider } from '@material-ui/core';
 
-import { sortMethodMenuItems } from '../constants/shared';
+import ListPostsPCHeader from './ListPostsPCHeader';
 
 class ListPostsPC extends Component {
 
-    state = {
-        sortMethod: ''
-    }
-
-    handleChange(event) {
-
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-
-        this.props.handleSelectSortMethod(event.target.value);
-    }
-
     render() {
 
-        const { selectedCategory } = this.props;
-
-        const { sortMethod } = this.state;
+        const { 
+            selectedCategory,
+            handleSelectSortMethod 
+        } = this.props;
 
         return (
             <div>
-                <Typography 
-                    variant="h3" 
-                    style={{
-                        paddingTop: '0.5em', 
-                        paddingBottom: '0.5em',
-                        paddingLeft: '0.5em'
-                    }}
-                >
-                    {selectedCategory.toUpperCase()}
-                    <span>
-                        <InputLabel 
-                            htmlFor="sort-method"
-                            style={{marginRight: '0.5em'}}
-                        >
-                            Sort by
-                        </InputLabel>
-                        <Select 
-                            value={sortMethod}
-                            onChange={this.handleChange.bind(this)}
-                            inputProps={{
-                                name: 'sortMethod',
-                                id: 'sort-method',
-                            }}
-                        >
-                            {sortMethodMenuItems.map(sMethod => (
-                                <MenuItem key={sMethod.key} value={sMethod.key}>
-                                    {sMethod.value}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </span>
-                </Typography>
+                <ListPostsPCHeader
+                    selectedCategory={selectedCategory}
+                    handleSelectSortMethod={handleSelectSortMethod}
+                />
                 <Divider />
             </div>
         );

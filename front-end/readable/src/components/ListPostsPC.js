d61f76851840c1bba382fcdfaecdb932f21054fc
yufrances90 <yufrances90@gmail.com>
2018-10-27 23:18:10 -0700

    feat: Added progress indicator when posts is undefined

--- front-end/readable/src/components/ListPostsPC.js
+++ front-end/readable/src/components/ListPostsPC.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
-import { Divider } from '@material-ui/core';
+import { 
+    Divider,
+    CircularProgress,
+    Tooltip
+} from '@material-ui/core';
 
 import ListPostsPCHeader from './ListPostsPCHeader';
 
@@ -10,7 +14,8 @@ class ListPostsPC extends Component {
 
         const { 
             selectedCategory,
-            handleSelectSortMethod 
+            handleSelectSortMethod,
+            posts
         } = this.props;
 
         return (
@@ -20,6 +25,17 @@ class ListPostsPC extends Component {
                     handleSelectSortMethod={handleSelectSortMethod}
                 />
                 <Divider />
+                <div className="app-circular-progress">
+                    {
+                        !posts && 
+                        <Tooltip 
+                            title="Please select sort method."
+                            placement="right"
+                        >
+                            <CircularProgress color="primary" /> 
+                        </Tooltip>
+                    }
+                </div>
             </div>
         );
     }

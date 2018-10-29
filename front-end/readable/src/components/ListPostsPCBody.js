be7f8da4476093568e866be1f254240906c311ca
yufrances90 <yufrances90@gmail.com>
2018-10-28 18:49:12 -0700

    feat: Added empty component for displaying a single post

--- front-end/readable/src/components/ListPostsPCBody.js
+++ front-end/readable/src/components/ListPostsPCBody.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 import PCPost from './PCPost';
 
+import {
+    List,
+    ListItem
+} from '@material-ui/core';
+
 class ListPostsPCBody extends Component {
     render() {
 
@@ -9,11 +14,24 @@ class ListPostsPCBody extends Component {
             posts
         } = this.props;
 
+        console.log(posts);
+
         return (
             <div>
-                {posts.map(post => (
-                    <PCPost post={post} />
-                ))}
+                { 
+                    posts.length <= 0 &&
+                    <p>No post is available for this category. Please try later!</p>
+                }
+                {
+                    posts.length > 0 &&
+                    <List>
+                        {posts.map(post => (
+                            <ListItem key={post.id}>
+                                <PCPost post={post} />
+                            </ListItem>
+                        ))}
+                    </List>
+                }
             </div>
         );
     }

165d3d2677b1409841a044692e63f7d3eb189e51
yufrances90 <yufrances90@gmail.com>
2018-11-15 21:45:27 -0800

    refactor: Minor change

--- front-end/readable/src/pages/PPost.js
+++ front-end/readable/src/pages/PPost.js
@@ -4,8 +4,7 @@ import { connect } from 'react-redux';
 
 import {
     LinearProgress,
-    Grid,
-    Button
+    Grid
 } from '@material-ui/core';
 
 import { 
@@ -138,9 +137,6 @@ class PPost extends Component {
                         />
                     </Grid>
                     <Grid item xs={3}>
-                        <Button onClick={this.handleVoteButtonClickForPost.bind(this)}>
-                            test
-                        </Button>
                     </Grid>
                 </Grid>
             </div>
